72e7ca6f55213038f63d37f886c884d99833fe67
mith99 <67262412+mith99@users.noreply.github.com>
2021-06-26 01:54:29 +0530

    login committed by Mith

--- src/IT19135830/Login/Login.js
+++ src/IT19135830/Login/Login.js
@@ -3,6 +3,10 @@ import {Button,Col, Row} from "reactstrap";
 import LoginCss from '../Stylesheets/login.css'
 // import {AiOutlineMail} from "react-icons/all";
 import {Mail} from 'react-feather';
+// import {login} from '../actions/auth';
+import {useDispatch} from "react-redux";
+import { Player, Controls } from '@lottiefiles/react-lottie-player';
+
 
 // import {Row} from "reactstrap";
 
@@ -10,8 +14,32 @@ import {Mail} from 'react-feather';
 class Login extends Component {
     constructor(props) {
         super(props);
+        this.onSubmit= this.onSubmit.bind(this);
+        this.onChange = this.onChange.bind(this);
+        this.state={
+            email:"",
+            password:""
+        }
+
+        // const dispatch = useDispatch();
+
 
     }
+
+
+
+
+    onSubmit(e){
+        e.preventDefault();
+        console.log(this.state);
+        //dispatch(login(this.state, history));
+    }
+
+
+    onChange(e){
+        this.setState({[e.target.name]:e.target.value})
+    }
+
 
     render(){
         return(
@@ -22,20 +50,40 @@ class Login extends Component {
                     <Col sm ='5'>
                         <h1 className='topic'>ICAF</h1>
                         <h3 className='subTopic'>International Conference on Application Frameworks</h3>
+
+                        <div>
+                            <Player
+                                autoplay
+                                loop
+                                src="https://assets4.lottiefiles.com/packages/lf20_jcikwtux.json"
+                                style={{ height: '40rem', width: '40rem', paddingTop:'2rem', paddingLeft:'2rem' }}
+                            >
+                                <Controls visible={false} buttons={['play', 'repeat', 'frame', 'debug']} />
+                            </Player>
+
+                        </div>
                     </Col>
                     <Col sm='4'>
                         <h3 className='login'>Login</h3>
                         <h3 className='instructions'>Please enter Email and Password to login</h3>
-                        <form>
+                        <form onSubmit={this.onSubmit}>
                             {/*<AiOutlineMail/>*/}
                             {/*<Mail size={25} className='mailIcon'/>*/}
                             <input className='inputfield'
                                    placeholder='Enter Email here'
+                                   name='email'
+                                   id='email'
+                                   value={this.state.email}
+                                   onChange={this.onChange}
                             >
                             </input>
                             {/*<Mail size={25}/>*/}
                             <input className='inputfield'
                                    placeholder='Enter Password here'
+                                   name = 'password'
+                                   id='password'
+                                   value={this.state.password}
+                                   onChange={this.onChange}
                             >
                             </input>
 
@@ -43,7 +91,7 @@ class Login extends Component {
 
                                 <Col sm='4'></Col>
 
-                                <button className ='loginbtn'>
+                                <button className ='loginbtn' type='submit'>
                                     <span className='btnTxt'>REGISTER</span>
                                 </button>
                                 <button className ='loginbtn'>
